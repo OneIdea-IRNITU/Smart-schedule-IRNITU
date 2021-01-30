d72a9cf130300efe42c9d3f67503ba7f72285069
bolanebyla <alexleskov2000@gmail.com>
2021-01-30 09:41:33 +0800

    Переименовать сбор статистики для tg

--- tg_bot/functions/storage.py
+++ tg_bot/functions/storage.py
@@ -84,7 +84,7 @@ class MongodbService(object):
         """возвращает расписание группы"""
         return self._db.schedule.find_one(filter={'group': group})
 
-    def save_statistics(self, action: str, date: str, time: str):
+    def save_tg_statistics(self, action: str, date: str, time: str):
         statistics = {
             'action': action,
             'date': date,
--- tg_bot/tools/statistics.py
+++ tg_bot/tools/statistics.py
@@ -4,4 +4,4 @@ from datetime import datetime
 def add(action: str, storage, tz):
     date_now = datetime.now(tz).strftime('%d.%m.%Y')
     time_now = datetime.now(tz).strftime('%H:%M')
-    storage.save_statistics(action=action, date=date_now, time=time_now)
+    storage.save_tg_statistics(action=action, date=date_now, time=time_now)
